4d0fb3db1ab45370161fcb71a48b6ce862f3f951
Ramiro Padro <rpadro@simtlix.com>
2022-02-16 14:22:39 -0300

    port

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.barsPath = '/api/bars';
         this.beersPath = '/api/beers';
         this.burgersPath = '/api/burgers';
